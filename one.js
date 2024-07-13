23f67e8fd3a2a9ad4fc8d9222a0333a58b1894b2
sdikshith <sdikshith7885@.com>
2024-07-13 13:42:16 +0530

    day one completed

---
+++ one.js
@@ -0,0 +1,75 @@
+// 30 Days - JavaScript Challenge
+
+// Day-1 
+
+// Task-1
+var a = 21
+console.log(a);
+
+// Task-2
+let b = "Hello SDikshith"
+console.log(b);
+
+// Task-3
+const c = true
+console.log(c);
+console.log(typeof(c));
+
+// Task-4
+var d = 21
+var e = "Hello"
+var f = true;
+var g = {id: 1, name: "Dikshith"};
+var h = [6,7,9]
+
+console.log(d,e,f,g,h);
+console.log(typeof(d),typeof(e),typeof(f),typeof(g),typeof(h));
+
+// Task-5
+let original = 21
+console.log("Orignal value: " + original);
+original = 11
+console.log("Re-assign value: " + original);
+
+// Task-6
+const init = 21
+console.log("Orignal value: " + original);
+init = 11 // const is an immutable variable
+console.log("Re-assign value: " + original);
+
+// -=----------------- Script for Feature Request -------------------------
+
+// Task-7 
+
+var number = 7
+console.log(number + " this is a type of: " + typeof(number));
+
+var string = "Hello World"
+console.log(string + " this is a type of: " + typeof(string));
+
+var boolean = false
+console.log(boolean + " this is a type of: " + typeof(boolean));
+
+var object = {id: 1, name: "Dikshith"}
+console.log(object + " this is a type of: " + typeof(object));
+
+var array = [1,2,3,4,5]
+console.log(array + " this is a type of: " + typeof(array));
+
+// Task-8
+// part-1 
+let change = 21
+console.log("Orignal value: " + change);
+change = 11
+console.log("Re-assign value: " + change); // IT can be mutable
+
+// part-2 
+const test = 7
+console.log("Orignal value: " + test);
+test = 8
+console.log("Re-assign value: " + test); // IT can be immutable and type Error
+
+
+
+
+// ------------------------ Day-1 Tasks Done ------------------------------
