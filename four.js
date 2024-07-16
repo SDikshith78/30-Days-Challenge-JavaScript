846ee16f7f5d3eddfb8f14f47fc2af3f3763c673
SDikshith <78164328+SDikshith78@users.noreply.github.com>
2024-07-16 21:35:12 +0530

    Day Four Completed

---
+++ four.js
@@ -0,0 +1,76 @@
+// 30 Days - JavaScript Challenge
+
+// Day-4
+
+// // Task-1 
+// for(var i = 1; i <=10; i++){
+//     console.log(i);
+// }
+
+// // Task-2
+// var a = 5
+// for(var i = 1 ; i <= 10 ; i++){
+//    var b = a*i
+//    console.log(`Multiplication of 5 X ${i} is: ${b}`);
+// }
+
+// // Task-3
+// let sum = 0
+// let i = 1
+// while(i <= 10){
+//     sum += i;
+//     i++
+// }
+// console.log(`Sum: ${sum}`);
+
+// Task-4
+// let i = 10
+// while(i>=1){
+//     console.log(i);
+//     i--
+// }
+
+// Task-5
+// let k = 1;
+// do {
+//     console.log(k);
+//     k++;
+// } while (k <= 5);
+
+// Task-6 
+// let num = 5;
+// let factorial = 1;
+// let l = num;
+// do {
+//     factorial *= l;
+//     l--;
+// } while (l > 0);
+// console.log(`Factorial of ${num}: ${factorial}`);
+
+// Task-7
+// for (let m = 1; m <= 5; m++) {
+//     let pattern = '';
+//     for (let n = 1; n <= m; n++) {
+//         pattern += '* ';
+//     }
+//     console.log(pattern);
+// }
+
+// Task-8 
+// for (let t = 1; t <= 10; t++) {
+//     if (t === 5) {
+//         continue;
+//     }
+//     console.log(t);
+// }
+
+// task-9 
+// for (let p = 1; p <= 10; p++) {
+//     if (p === 7) {
+//         break;
+//     }
+//     console.log(p);
+// }
+
+
+// --------------------------------Day 4 Done ------------------------
